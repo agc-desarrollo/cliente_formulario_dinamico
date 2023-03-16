55805c55e10f9bfa650d0f138c11f338f7576a93
Luciano <lucho.2012.tandil@gmail.com>
2023-03-16 03:44:25 -0300

    Se agrega script para recrear enlaces simbólicos para pruebas de desarrollo

---
+++ develop/genera_ln.sh
@@ -0,0 +1,4 @@
+rm ./node_modules/@agc-desarrollo/cliente_formulario_dinamico/dist/library.js
+rm ./node_modules/@agc-desarrollo/cliente_formulario_dinamico/dist/library.mjs
+ln ../dist/library.mjs ./node_modules/@agc-desarrollo/cliente_formulario_dinamico/dist/library.mjs
+ln ../dist/library.js ./node_modules/@agc-desarrollo/cliente_formulario_dinamico/dist/library.js 
